22f39fcd93b64b0c47cfc14ac022653a87114f8d
Kasun Vithanage <alankasun@gmail.com>
2022-02-07 23:33:29 +0530

    update knexfile to work in prod mode

--- knexfile.ts
+++ knexfile.ts
@@ -1,12 +1,26 @@
 // eslint-disable-next-line @typescript-eslint/no-var-requires
-require('dotenv').config();
+require('dotenv').config({ path: require('find-config')('.env') });
+
+const isProd =
+  process.env.NODE_CONFIG_ENV === 'production' ||
+  process.env.NODE_CONFIG_ENV === 'staging' ||
+  process.env.NODE_ENV === 'production' ||
+  process.env.NODE_ENV === 'staging';
+
+const extension = isProd ? 'js' : 'ts';
 
 module.exports = {
   client: 'pg',
   connection: process.env.DATABASE_URL,
   migrations: {
     directory: './src/database/migrations',
-    extension: 'ts',
+    extension: extension,
+    loadExtensions: [`.${extension}`],
     stub: './src/database/stubs/migration.stub',
+  },
+  seeds: {
+    directory: './src/database/seeds',
+    extension: extension,
+    loadExtensions: [`.${extension}`],
   },
 };
