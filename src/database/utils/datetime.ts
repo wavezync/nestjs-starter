ef53ee71ea7af9c07e921ac6ece9a1c77fba2eee
samaratungajs <samaratungajs@gmail.com>
2025-02-06 20:55:40 +0530

    fix: timestamps data types updated

--- src/database/utils/datetime.ts
+++ src/database/utils/datetime.ts
@@ -4,14 +4,14 @@ export const withTimestamps = <T>(
     createdAt = true,
     updatedAt = true,
   }: { createdAt?: boolean; updatedAt?: boolean } = {},
-): T & { created_at: number; updated_at: number } => {
-  const now = +new Date();
+): T & { created_at: Date; updated_at: Date } => {
+  const now = new Date();
 
   return {
     ...data,
     ...(createdAt ? { created_at: now } : {}),
     ...(updatedAt ? { updated_at: now } : {}),
-  } as T & { created_at: number; updated_at: number };
+  } as T & { created_at: Date; updated_at: Date };
 };
 
 export type UpdateWithoutTimestamps<T> = Omit<T, 'updated_at'>;
